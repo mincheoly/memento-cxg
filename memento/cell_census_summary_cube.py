68597006a17cb22f192dc6bf888601f1a0f88c4e
Andrew Tolopko <atolopko@chanzuckerberg.com>
2023-03-30 14:43:44 -0400

    use multiprocessing for estimator computation

--- memento/cell_census_summary_cube.py
+++ memento/cell_census_summary_cube.py
@@ -1,24 +1,38 @@
-import itertools
+import concurrent
+import logging
+import multiprocessing
 import sys
+from concurrent import futures
 
 import cell_census
 import pandas as pd
 import scipy.sparse
 import scipy.sparse
-import somacore
+import tiledbsoma as soma
 from somacore import ExperimentAxisQuery, AxisQuery
 
 from estimators import compute_mean, compute_sem, bin_size_factor, compute_variance, compute_sev
 
-cube_dims_obs = [
+MIN_BATCH_SIZE = 1000
+
+CUBE_DIMS_OBS = [
     "cell_type",
     "dataset_id",
 ]
-cube_dims = ["gene_ontology_term_id"] + cube_dims_obs
+CUBE_DIMS = ['feature_id'] + CUBE_DIMS_OBS
 
-estimator_names = ['n', 'mean', 'sem', 'var', 'sev', 'selv']
+ESTIMATOR_NAMES = ['n', 'mean', 'sem', 'var', 'sev', 'selv']
 
 Q = 0.1  # RNA capture efficiency depending on technology
+
+MAX_WORKERS = None  # None means use multiprocessing's dynamic default
+
+logging.basicConfig(
+    format="%(asctime)s %(process)-7s %(levelname)-8s %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logging.captureWarnings(True)
 
 
 def compute_all_estimators(grouped):
@@ -35,25 +49,24 @@ def compute_all_estimators(grouped):
     return pd.Series(data=[n, mean, sem, variance, sev, selv])
 
 
-def batched(iterable, n):
-    it = iter(iterable)
-    while batch := tuple(itertools.islice(it, n)):
-        yield batch
+def compute_all_estimators_for_batch(soma_dim_0, obs_df: pd.DataFrame, var_df: pd.DataFrame, X_uri: str):
+    with soma.SparseNDArray.open(X_uri) as X:
+        X_df = X.read(coords=(soma_dim_0,
+                              var_df.index.values)).tables().concat().to_pandas()
+        logging.info(f"Pass 2: Processing X batch cells={len(soma_dim_0)}, nnz={len(X_df)}")
+        # Compute estimators for each gene
+        return (
+            X_df.merge(var_df['feature_id'], left_on='soma_dim_1', right_index=True).
+            merge(obs_df[CUBE_DIMS_OBS + ['approx_size_factor']], left_on='soma_dim_0', right_index=True).
+            drop(columns=['soma_dim_0', 'soma_dim_1']).
+            groupby(CUBE_DIMS, sort=False).
+            apply(compute_all_estimators).
+            rename(mapper=dict(enumerate(ESTIMATOR_NAMES)), axis=1)
+        )
 
 
-def compute_estimators_for_batch(soma_dim_0):
-    X_df = X.read(coords=(soma_dim_0,
-                          query.var_joinids())).tables().concat().to_pandas()
-    print(f"Pass 2: Processing X batch cells={len(soma_dim_0)}, nnz={len(X_df)}")
-    # Compute estimators for each gene
-    return (
-        X_df.merge(var_df['feature_id'], left_on='soma_dim_1', right_index=True).
-        merge(obs_df[cube_dims_obs + ['approx_size_factor']], left_on='soma_dim_0', right_index=True).
-        drop(columns=['soma_dim_0', 'soma_dim_1']).
-        groupby(cube_dims_obs + ['feature_id'], sort=False).
-        apply(compute_all_estimators).
-        rename(mapper=dict(enumerate(estimator_names)), axis=1)
-    )
+def no_op():
+    return pd.DataFrame(data=[[1] * len(ESTIMATOR_NAMES)], columns=ESTIMATOR_NAMES)
 
 
 if __name__ == "__main__":
@@ -66,12 +79,12 @@ if __name__ == "__main__":
     with ExperimentAxisQuery(organism_census,
                              measurement_name="RNA",
                              obs_query=AxisQuery(),  #value_filter="cell_type=='plasma cell'"),
-                             var_query=AxisQuery(coords=(slice(0, 10000),))) as query:
+                             var_query=AxisQuery(coords=(slice(0, 1000),))) as query:
         var_df = query.var().concat().to_pandas().set_index("soma_joinid")
         var_df['feature_id'] = var_df['feature_id'].astype('category')
 
         obs_df = (
-            query.obs(column_names=["soma_joinid"] + cube_dims_obs).
+            query.obs(column_names=["soma_joinid"] + CUBE_DIMS_OBS).
             concat().
             to_pandas().
             set_index("soma_joinid")
@@ -80,10 +93,10 @@ if __name__ == "__main__":
         obs_df['cell_type'] = obs_df['cell_type'].astype('category')
         obs_df['size_factor'] = 0  # accumulated
 
-        print(f"Pass 1: Compute Approx Size Factors")
+        logging.info(f"Pass 1: Compute Approx Size Factors")
 
         for X_tbl in query.X("raw").tables():
-            print(f"Pass 1: Processing X batch nnz={X_tbl.shape[0]}")
+            logging.info(f"Pass 1: Processing X batch nnz={X_tbl.shape[0]}")
             X_df = X_tbl.to_pandas()
 
             # Sum all gene expression levels for each cell
@@ -95,31 +108,52 @@ if __name__ == "__main__":
         # Bin all sums to have fewer unique values, to speed up bootstrap computation
         obs_df['approx_size_factor'] = bin_size_factor(obs_df['size_factor'].values)
 
-        print(f"Pass 2: Compute Estimators")
+        logging.info(f"Pass 2: Compute Estimators")
 
         # accumulate into feature_id/cell_type/dataset_id Pandas multi-indexed DataFrame
-        cube_index = pd.MultiIndex.from_arrays([[]] * 3, names=cube_dims_obs + ['feature_id'])
-        cube = pd.DataFrame(index=cube_index, columns=estimator_names)
+        cube_index = pd.MultiIndex.from_arrays([[]] * 3, names=CUBE_DIMS)
+        cube = pd.DataFrame(index=cube_index, columns=ESTIMATOR_NAMES)
 
         # Process X by cube rows. This ensures that estimators are computed
         # for all X data contributing to a given cube row aggregation.
-        X: somacore.SparseNDArray = query.experiment.ms['RNA'].X['raw']
         # TODO: `groups` converts categoricals to strs, which is inefficient
-        cube_coords = obs_df[cube_dims_obs].groupby(cube_dims_obs).groups
+        cube_coords = obs_df[CUBE_DIMS_OBS].groupby(CUBE_DIMS_OBS).groups
         soma_dim_0_batch = []
 
-        for group_key, soma_dim_0_row in cube_coords.items():
+        if multiprocessing.get_start_method(True) != "spawn":
+            multiprocessing.set_start_method("spawn", True)
+
+        ppe = futures.ProcessPoolExecutor(max_workers=MAX_WORKERS)
+
+        batch_futures = []
+
+        for soma_dim_0_row in cube_coords.values():
+            soma_dim_0_batch.extend(soma_dim_0_row)
+
             # Fetch data for multiple cube rows at once, to reduce X.read() call count
-            if len(soma_dim_0_batch) < 10000:
-                soma_dim_0_batch.extend(soma_dim_0_row)
+            if len(soma_dim_0_batch) < MIN_BATCH_SIZE:
                 continue
 
-            # TODO: Parallelize
-            cube = cube.append(compute_estimators_for_batch(soma_dim_0_batch))
+            batch_futures.append(ppe.submit(compute_all_estimators_for_batch,
+                                            soma_dim_0_batch,
+                                            obs_df,
+                                            var_df,
+                                            query.experiment.ms['RNA'].X['raw'].uri))
             soma_dim_0_batch = []
 
         # Process final batch
-        cube = cube.append(compute_estimators_for_batch(soma_dim_0_batch))
+        if len(soma_dim_0_batch) > 0:
+            batch_futures.append(ppe.submit(compute_all_estimators_for_batch,
+                                            soma_dim_0_batch,
+                                            obs_df,
+                                            var_df,
+                                            query.experiment.ms['RNA'].X['raw'].uri))
+
+        for n, future in enumerate(concurrent.futures.as_completed(batch_futures), start=1):
+            result = future.result()
+            cube = cube.append(result, ignore_index=True)
+            logging.info(f"Pass 2: Completed [{n} of {len(batch_futures)}]")
+
         # TODO: Write to disk (e.g. as TileDB Array)
         print(cube)
 
