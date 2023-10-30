5547edaee57307d9b7e03e58182e32b6c11c9168
Andrew Tolopko <atolopko@chanzuckerberg.com>
2023-10-30 08:12:42 -0400

    add cells active stat to batch-timing.py

--- batch-timings.py
+++ batch-timings.py
@@ -8,6 +8,8 @@ output_lines = []
 # Dictionary to store start times and nnz values of batch processes
 start_times = {}
 
+cells_active = nnz_active = 0
+
 for line in sys.stdin:
     parts = line.split()
     if len(parts) < 11:
@@ -18,27 +20,36 @@ for line in sys.stdin:
     # Use regular expressions to extract the batch ID and nnz value
     batch_match = re.search(r'batch (\d+)', line)
     nnz_match = re.search(r'nnz=(\d+)', line)
+    cells_match = re.search(r'cells=(\d+)', line)
 
     if batch_match:
         batch_id = int(batch_match.group(1))
     else:
         continue
 
+    if "Start" in line or "End" in line:
+        cells = int(cells_match.group(1)) if cells_match else None
+        nnz = int(nnz_match.group(1)) if nnz_match else None
+
     if "Start" in line:
         # Store the start time and nnz value (if available)
         start_times[batch_id] = {"start_time": datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S"), "nnz_value": nnz_match.group(1) if nnz_match else None}
+        cells_active += cells
+        nnz_active += nnz
 
     elif "End" in line:
         if batch_id in start_times:
             end_time = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
             elapsed_time = end_time - start_times[batch_id]["start_time"]
-            nnz_value = int(nnz_match.group(1)) if nnz_match else None
 
-            # Calculate nnz per second
-            nnz_per_second = nnz_value / elapsed_time.total_seconds() if nnz_value else None
+            # Calculate rates
+            nnz_per_second = nnz / elapsed_time.total_seconds() if nnz else None
+            cells_per_second = cells / elapsed_time.total_seconds() if cells else None
 
             # Store the output line in the list
-            output_line = f"Batch {batch_id}: Elapsed time: {elapsed_time}, nnz: {nnz_value // 1000000}M, nnz per second: {nnz_per_second:.2f}"
+            output_line = (f"Batch {batch_id}: Elapsed time: {elapsed_time}, "
+                           f"cells: {cells}, cells/sec: {cells_per_second:.2f}, "
+                           f"nnz: {nnz // 1000000}M, nnz/sec: {nnz_per_second:.2f}")
             output_lines.append((elapsed_time, output_line))
             del start_times[batch_id]
 
